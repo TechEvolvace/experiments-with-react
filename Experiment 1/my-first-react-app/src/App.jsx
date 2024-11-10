8c35a19bb55865dc8e5aa4360d7580cb919370fa
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-11-09 22:08:06 -0600

    Added comments for both App.jsx and Counter.jsx to help understand the process of these JSX file for a React project!

--- Experiment 1/my-first-react-app/src/App.jsx
+++ Experiment 1/my-first-react-app/src/App.jsx
@@ -1,9 +1,27 @@
+/* 
+  Imported the styles from App.css file 
+  and the Counter component with its functionality 
+  from the Counter.jsx file!
+*/
 import './App.css'
 import Counter from './Counter'
 
 function App() {
 
+  /* 
+    The App function in the App.jsx file loads this 
+    larger component with all the elements to display 
+    for the website, which includes the headings text
+    and the multiple Counter components! 
+
+    The App function is exported and imported to the 
+    main.jsx file!
+  */
   return (
+    /*
+      Larger container that contains all of the content to display 
+      for the website is what is returned by this App function! 
+    */
     <div className="App">
       <h1>First React Experiment</h1>
       <h2>Counter 1: </h2>
