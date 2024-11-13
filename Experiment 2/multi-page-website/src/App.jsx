f42e389312aa6e23826ddd14d5e1b876c3f23827
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-11-12 22:59:09 -0600

    Created new Webpages folder and new React Components for Home, About, and Process pages!

--- Experiment 2/multi-page-website/src/App.jsx
+++ Experiment 2/multi-page-website/src/App.jsx
@@ -1,10 +1,13 @@
 // import './App.css'
 
+import {Home, About, Process} from './Webpages'
+
 function App() {
 
   return (
     <>
       <h1>Experiment 2: Multipage Website with Functional Page Routing!</h1>
+      <About />
     </>
   )
 }
---
+++ Experiment 2/multi-page-website/src/Webpages/About.jsx
@@ -0,0 +1,10 @@
+
+export default function About(){
+
+    /* Returns all needed content to display in About page */
+    return (
+        <>
+            <h1>Welcome to the About page!</h1>
+        </>
+    )
+}
