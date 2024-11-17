b20995da88afb0a8d3b89e5e7b15ac48f2b5e18e
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-11-16 22:41:43 -0600

    Imported all of the webpages components from new webpages.js file!

--- Experiment 2/multi-page-website/src/App.jsx
+++ Experiment 2/multi-page-website/src/App.jsx
@@ -1,10 +1,19 @@
 // import './App.css'
 
+// This works because the index.js inside Webpages export the React components to here. 
+// import {Home, About, Process} from "./Webpages";
+
+// This works because it directly access the Home component stored in this location specified in this absolute path.
+import {Home, About, Process} from "./Webpages/webpages.js";
+
 function App() {
 
   return (
     <>
       <h1>Experiment 2: Multipage Website with Functional Page Routing!</h1>
+      <Home />
+      <About />
+      <Process />
     </>
   )
 }
