89202b937592fec713ab2b9953ae68c968c73647
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-11-16 22:40:00 -0600

    Updated new content for the Home and Process page in their new locations!

--- Experiment 2/multi-page-website/src/Webpages/Home.jsx
+++ Experiment 2/multi-page-website/src/Webpages/Home.jsx
@@ -1,1 +1,18 @@
 /* Currently not used yet */
+
+import { HomeTitle } from "./Home_Components/home.js";
+
+const Home = () => {
+
+    /* Returns all needed content to display in Home page */
+    return (
+        <>
+            <h1>Welcome to the Home page!</h1>
+            <HomeTitle message={"Home Title successfully implemented!"} />
+
+            <HomeTitle message={"We got the props verified!"} />
+        </>
+    );
+};
+
+export default Home;
--- Experiment 2/multi-page-website/src/Webpages/Process.jsx
+++ Experiment 2/multi-page-website/src/Webpages/Process.jsx
@@ -1,1 +1,12 @@
 /* Currently not used yet */
+const Process = () => {
+
+    /* Returns all needed content to display in Process page */
+    return (
+        <>
+            <h1>Welcome to the Process page!</h1>
+        </>
+    )
+}
+
+export default Process;
