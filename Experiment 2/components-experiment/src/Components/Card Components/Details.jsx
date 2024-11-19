bc0edda5cf809b0f7e1e6c910b4417d1f8e93562
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-11-18 23:47:02 -0600

    Successfully rendered the Details component inside the Experiment Card component!

---
+++ Experiment 2/components-experiment/src/Components/Card Components/Details.jsx
@@ -0,0 +1,18 @@
+const Details = () => {
+
+    return (
+        <>
+            <h2 style={{fontSize: "1.3em"}}>
+                Goals of Experiment 2: 
+            </h2>
+            <ol style={{fontSize: "1.1em"}}>
+                <li><p>1</p></li>
+                <li><p>2</p></li>
+                <li><p>3</p></li>
+            </ol>
+        </>
+    )
+
+}
+
+export default Details;
--- Experiment 2/components-experiment/src/Components/ExperimentCard.jsx
+++ Experiment 2/components-experiment/src/Components/ExperimentCard.jsx
@@ -1,9 +1,21 @@
 import "./ExperimentCard.css"
 
+/* 
+    Also possible to import components exported from any 
+    JavaScript file from anywhere, including inside
+    subfolders, as long as you provide the correct path
+    to that JavaScript file! 
+
+    Imports the Details component from the Card Components.js
+    file inside the Card Components folder! 
+*/
+import { Details } from "./Card Components/Card Components.js"
+
 const Experiment_Card = () => {
     return (
         <>
             <h2>Experiment Card</h2>
+            <Details />
         </>
     )
 }
