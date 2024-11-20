e5f71ecaec19c6b7fe98b4e49f530e2da4da07d9
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-11-19 22:14:29 -0600

    Added addiitonal details about each component used for Experiment 2 in Details.jsx file!

--- Experiment 2/components-experiment/src/Components/Card Components/Details.jsx
+++ Experiment 2/components-experiment/src/Components/Card Components/Details.jsx
@@ -26,6 +26,40 @@ const Details = () => {
                     </p>
                 </li>
             </ol>
+            <h2 style={{fontSize: "1.3em"}}>
+                Components:
+            </h2>
+            <ul style={{fontSize: "1.1em"}}>
+                <li>
+                    <h3 style={{color: "green"}}>App (exported from App.jsx)</h3>
+                    <p>Function: responsible for displaying content of website application!</p>
+                    <p>Path: /Experiment 2/components-experiment/src/App.jsx</p>
+                    <p>Imports Title and ExperimentCard components</p>
+                    <p>Also imports React by default due to using React version 18!</p>
+                </li>
+                <li>
+                    <h3 style={{color: "green"}}>Title (exported from Title.jsx)</h3>
+                    <p>Function: displays Title for Experiment 2 on the website with an underline in green color</p>
+                    <p>Path: /Experiment 2/components-experiment/src/Components/Title.jsx</p>
+                    <p>Imports React by default due to using React version 18!</p>
+                    <p>Uses inline-CSS styling to apply styling for Title component!</p>
+                </li>
+                <li>
+                    <h3 style={{color: "green"}}>ExperimentCard (exported from ExperiemntCard.jsx)</h3>
+                    <p>Function: Contains the details of the experiment in a card displayed on the website</p>
+                    <p>Path: /Experiment 2/components-experiment/src/Components/ExperimentCard.jsx</p>
+                    <p>Imports Details component</p>
+                    <p>Also imports ExperimentCard.css file to apply styling for ExperimentCard component!</p>
+                    <p>Also imports React by default due to using React version 18!</p>
+                </li>
+                <li>
+                    <h3 style={{color: "green"}}>Details (exported from Details.jsx)</h3>
+                    <p>Function: Content of the details for Experiment 2 that gets displayed on the website!</p>
+                    <p>Path: /Experiment 2/components-experiment/src/Components/Card Components/Card Components.jsx</p>
+                    <p>Imports React by default due to using React version 18!</p>
+                    <p>Uses inline-CSS styling to apply styling for Details component!</p>
+                </li>
+            </ul>
         </>
     )
 
