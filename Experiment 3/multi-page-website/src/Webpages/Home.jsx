43626046c6da417c0340b8db930efc6f006fafd0
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-12-07 23:43:31 -0600

    Figured out how to apply classes and ID selectors styling with CSS Modules!

--- Experiment 3/multi-page-website/src/Webpages/Home.jsx
+++ Experiment 3/multi-page-website/src/Webpages/Home.jsx
@@ -1,17 +1,31 @@
 /* Currently not used yet */
 
 import { HomeTitle } from "./Home_Components/home.js";
-import './Home.css';
+import styles from './Home.module.css';
 
 const Home = () => {
 
     /* Returns all needed content to display in Home page */
     return (
         <>
-            <h1 id="title">Welcome to the Home page!</h1>
+            <h1 id={`${styles.title}`}>Welcome to the Home page!</h1>
+            <p className={`${styles.content}`}>
+                The home page is the landing page for React Experiment 3 website!
+            </p>
+            <div>
+                <p className={`${styles.content}`}>
+                    Currently, we need to include a real navbar with navigation links,
+                    but you can adjust the current URL of this website to a specific 
+                    webpage by adding the following:    
+                </p>
+                <ul className={`${styles.content}`}>
+                    <li>For the Home page: add /</li>
+                    <li>For the About page: add /About</li>
+                    <li>For the Process page: add /Process</li>
+                </ul>
+            </div>
+             
             <HomeTitle message={"Home Title successfully implemented!"} />
-
-            <HomeTitle message={"We got the props verified!"} />
         </>
     );
 };
