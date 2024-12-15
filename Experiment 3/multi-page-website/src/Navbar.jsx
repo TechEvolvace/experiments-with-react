c6661d32c2e0f47860d376ab9e02f1a2c8bed892
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-12-14 23:33:50 -0600

    Implemented the new navigation button design!

--- Experiment 3/multi-page-website/src/Navbar.jsx
+++ Experiment 3/multi-page-website/src/Navbar.jsx
@@ -1,24 +1,23 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
     
     return (
-        <>
-            <nav>
-                {/* Navigation Links */}
-                <Link to="/">
-                    Home
-                </Link>
+        <nav>
+            {/* Navigation Links */}
+            <Link to="/" className="link-button" >
+                Home
+            </Link>
 
-                <Link to="/About">
-                    About
-                </Link>
+            <Link to="/About" className="link-button">
+                About
+            </Link>
 
-                <Link to="/Process">
-                    Process
-                </Link>
-            </nav>
-        </>
+            <Link to="/Process" className="link-button">
+                Process
+            </Link>
+        </nav>
     )
 }
 
