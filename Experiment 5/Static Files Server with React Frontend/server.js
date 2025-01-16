c491531c23adab6abd4002b99a53855cdec92538
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-01-15 22:39:27 -0600

    Setting up route toserver index.html for all other routes different from the route that server static files from the public directory!

--- Experiment 5/Static Files Server with React Frontend/server.js
+++ Experiment 5/Static Files Server with React Frontend/server.js
@@ -1,13 +1,18 @@
 // Import Express and create an Express server app
 const express = require('express');
+const path = require('path');
 const app = express();
 
 // Serve static files from the public directory
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-// Defining base route
-app.get('/', function(req, res) {
-    res.send(`Welcome to the Static Files Server with React Frontend!`)
+
+/* 
+    Handle React routing, return all requests
+    to React app
+*/
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server 
